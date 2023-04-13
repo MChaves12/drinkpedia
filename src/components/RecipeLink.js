8d2fdb515c880991d0a7888d7c9433d0321e0699
Natalia Nossack <nvcordeiro22@gmail.com>
2023-04-13 14:20:21 +0200

    corrigi alguns erros gramaticais

---
+++ src/components/RecipeLink.js
@@ -0,0 +1,14 @@
+
+function RecipeLink ({drinkIMG, drinkName}) {
+    return(
+        <div className='recipeLink-container'>
+            <img src={drinkIMG} alt='drink-name'></img>
+            <hr></hr>
+            <p>{drinkName}</p>
+            <hr></hr>
+            <button className='recipeLinkBtn'>Show me the Recipe</button>
+        </div>
+    );
+};
+
+export default RecipeLink;
