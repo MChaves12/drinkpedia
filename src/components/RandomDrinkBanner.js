9b268924f49af648bf2f28239cef293f3199e7b1
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-04-14 12:03:49 -0300

    Estilização da Home iniciada

--- src/components/RandomDrinkBanner.js
+++ src/components/RandomDrinkBanner.js
@@ -1,19 +1,19 @@
 import { Link } from 'react-router-dom'
+import bannerImg from '../assets/barman.jpg';
+
 
 function RandomDrinkBanner () {
     return(
-        <>
-        <Link className='drink-link'>
-            <div className='drink-recomendado-container'>
-                <div className='img-filter'>
-                    <div className='drink-recomendado-info'>
-                        <h4>Sugestão do Bartender</h4>
-                        <h1>Drink Name</h1>
-                    </div>
-                </div>
+        <div className='sujestao-container'>
+            <div>
+                <img className='sujestao-img' src={bannerImg} alt='barman'></img>
             </div>
-        </Link>
-        </> 
+            <div className='sujestao-description'>
+                <h2>Não sabe por onde começar?</h2>
+                <h4>Veja uma sujestão do nosso bartender.</h4>
+                <Link className=''><button className='sujestao-btn'>Me indique um drink</button></Link>
+            </div>
+        </div> 
     );
 };
 
